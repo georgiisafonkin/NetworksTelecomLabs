b115ae1eda6103ba98011592afe38c1a004c3ee7
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-15 16:52:52 +0700

    structure init, threads funcs declaration

--- lab1/main.c
+++ lab1/main.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
+#include <arpa/inet.h>
+#include<stdbool.h>
+
+typedef struct {
+    char ip[INET6_ADDRSTRLEN];
+    bool is_active;
+};
+
+peer_t peers[100];
+int peer_count = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* peers_listener_func(void* args);
+void* peers_communicator_func(void * args);
 
 int main(int argc, char* argv[]) {
-
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <multicast_address>\n", argv[0]);
+    }
     return EXIT_SUCCESS;
+}
+
+void* peers_listener_func(void* args) {
+
 }
