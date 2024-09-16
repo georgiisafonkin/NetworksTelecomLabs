1216dd54c56b9724d463b962dc44cba56a821762
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-16 13:08:12 +0700

    listener_func changes

--- lab1/main.c
+++ lab1/main.c
@@ -1,8 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <arpa/inet.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
+#include <error.h>
+
+
 
 typedef struct {
     char ip[INET6_ADDRSTRLEN];
@@ -24,5 +33,24 @@ int main(int argc, char* argv[]) {
 }
 
 void* peers_listener_func(void* args) {
+    int socket_fd;
+    struct addrinfo hints, *result;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(NULL, "12345", &hints, &result) != 0) {
+        perror("getaddrinfo");
+        pthread_exit(NULL);
+    }
+
+    socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (socket_fd == -1) {
+        perror("socket");
+        freeaddrinfo(result);
+        pthread_exit(NULL);
+    }
+
 
 }
