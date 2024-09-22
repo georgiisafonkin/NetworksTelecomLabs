8e98e3762500509a6598d659fd67b48da6e67dfe
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-22 14:46:26 +0700

    join_multicast_group func added

--- lab1/main.c
+++ lab1/main.c
@@ -30,8 +30,8 @@ void* peers_listener_func(void* args);
 void* peers_speaker_func(void * args);
 
 void handle_error(const char* error_msg);
-int is_valid_ipv4_address(const char* address);
-
+int is_valid_ipv4_addr(const char* address);
+int is_valid_ipv6_addr(const char* address);
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -62,7 +62,29 @@ int configure_socket(int socket_fd) {
 }
 
 int join_multicast_group(int socket_fd, const char* group_address) {
-    struct ipv6_mreq
+    if (is_valid_ipv4_addr(group_address)) {
+        struct ip_mreq mreq;
+
+        if (inet_pton(AF_INET, group_address, &(mreq.imr_multiaddr)) != 1) {
+            handle_error("inet_pton");
+        }
+
+        if (setsockopt(socket_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) != 0) {
+            handle_error("can't join multicast group");
+        }
+    } else if (is_valid_ipv6_addr(group_address)) {
+        struct ipv6_mreq mreq6;
+
+        if (inet_pton(AF_INET, group_address, &(mreq6.ipv6mr_multiaddr)) != 1) {
+            handle_error("inet_pton");
+        }
+
+        if (setsockopt(socket_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq6, sizeof(mreq6)) != 0) {
+            handle_error("can't join multicast group");
+        }
+    } else {
+        handle_error("invalid ip address");
+    }
 }
 
 void* peers_listener_func(void* args) {
@@ -78,13 +100,13 @@ void handle_error(const char* error_msg) {
     exit(EXIT_FAILURE);
 }
 
-int is_valid_ipv4_address(const char* address) {
+int is_valid_ipv4_addr(const char* address) {
     struct sockaddr_in sa;
     int result = inet_pton(AF_INET, address, &(sa.sin_addr));
     return result == 1;
 }
 
-int is_valid_ipv6_address(const char* address) {
+int is_valid_ipv6_addr(const char* address) {
     struct sockaddr_in sa;
     int result = inet_pton(AF_INET6, address, &(sa.sin_addr));
     return result == 1;
