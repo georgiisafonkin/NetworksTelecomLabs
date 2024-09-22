793dce7f6848e4652673334303c2a21b70d4e41c
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-22 14:29:17 +0700

    is_valid_ip_addr funcs added

--- lab1/main.c
+++ lab1/main.c
@@ -22,13 +22,20 @@ peer_t peers[100];
 int peer_count = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void handle_error(const char* error_msg);
+int create_socket(int domain, int type, int protocol);
+int configure_socket(int socket_fd);
+int join_multicast_group(int socket_fd, const char* group_address);
+
 void* peers_listener_func(void* args);
 void* peers_speaker_func(void * args);
 
+void handle_error(const char* error_msg);
+int is_valid_ipv4_address(const char* address);
+
+
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <multicast_address> <port>\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <multicast_address>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -43,144 +50,42 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-void* peers_listener_func(void* args) {
-    char** argv = (char**)args;
-    char* multicast_group_address = argv[1];
-    char* port = argv[2];
-    printf("Readed port: %s\n", port);
-    char buffer[BUFFER_SIZE];
-    int socket_fd;
-    struct addrinfo hints, *result;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    //receiving local area network addresses
-    if (getaddrinfo(NULL, port, &hints, &result) != 0) { //TODO SMTH WITH PORT
-        perror("getaddrinfo");
-        pthread_exit(NULL);
-    }
-
-    //receiving socket file descriptor
-    socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+int create_socket(int domain, int type, int protocol) {
+    int socket_fd = socket(domain, type, protocol);
     if (socket_fd == -1) {
-        perror("socket");
-        freeaddrinfo(result);
-        pthread_exit(NULL);
+        handle_error("can't create a socket");
     }
+}
 
-    //connecting to multicast group
-    if (result->ai_family == AF_INET) {
-        struct ip_mreq ipv4_mreq;
-        //assigning multicast-group address in byte as binary data in network byte order
-        ipv4_mreq.imr_multiaddr.s_addr = inet_addr(multicast_group_address); //TODO it with aton later, because it more modern and safe
-        //idk why it is here
-        ipv4_mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-        //set options from "mreq" structure to our socket
-        setsockopt(socket_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &ipv4_mreq, sizeof(ipv4_mreq));
-    } else if (result->ai_family == AF_INET6) {
-        struct ipv6_mreq ipv6_mreq;
-        inet_pton(AF_INET6, multicast_group_address, &ipv6_mreq.ipv6mr_multiaddr);
-        ipv6_mreq.ipv6mr_interface = 0; //default interface
-        setsockopt(socket_fd, IPPROTO_IPV6, IPV6_JOIN_GROUP, &ipv6_mreq, sizeof(ipv6_mreq));
-    }
+int configure_socket(int socket_fd) {
 
-    bind(socket_fd, result->ai_addr, result->ai_addrlen);
-    freeaddrinfo(result);
+}
 
-    //listening...
-    while (1) {
-        socklen_t addr_len = sizeof(struct sockaddr_storage);
-        struct sockaddr_storage addr;
-        int n = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &addr, &addr_len);
-        if (n < 0) {
-            perror("recvfrom");
-            continue;
-        }
-        buffer[n] = '\0';
-        printf("%s\n", buffer);
+int join_multicast_group(int socket_fd, const char* group_address) {
+    struct ipv6_mreq
+}
 
-        char ip[INET6_ADDRSTRLEN];
-        if (addr.ss_family == AF_INET) {
-            struct sockaddr_in *s = (struct sockaddr_in *) &addr;
-            inet_ntop(AF_INET, &s->sin_addr, ip, sizeof(ip));
-        } else {
-            struct sockaddr_in6 *s = (struct sockaddr_in6 *) &addr;
-            inet_ntop(AF_INET6, &s->sin6_addr, ip, sizeof(ip));
-        }
+void* peers_listener_func(void* args) {
 
-        //updating list of peers
-        pthread_mutex_lock(&mutex);
-
-        int found = 0;
-
-        for (int i = 0; i < peer_count; ++i) {
-            if (strcmp(peers[i].ip, ip) == 0) {
-                peers[i].is_active = true;
-                found = 1;
-                break;
-            }
-        }
-
-        if (!found && peer_count < 100) {
-            strcpy(peers[peer_count].ip, ip);
-            peers[peer_count].is_active = true;
-            ++peer_count;
-        }
-
-        pthread_mutex_unlock(&mutex);
-
-        //output the list of active peers
-        printf("active copies:\n");
-        pthread_mutex_lock(&mutex);
-        for (int i = 0; i < peer_count; ++i) {
-            if (peers[i].is_active) {
-                printf("%s:%s\n", peers[i].ip, port);
-            }
-        }
-        pthread_mutex_unlock(&mutex);
-    }
-
-    close(socket_fd);
-    pthread_exit(NULL);
 }
 
 void* peers_speaker_func(void * args) {
-    char** argv = (char**)args;
-    char* multicast_group_address = argv[1];
-    char* port = argv[2];
-    int socket_fd;
-    struct addrinfo hints, *result;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-
-    if (getaddrinfo(multicast_group_address, port, &hints, &result) != 0) { //TODO SMTH WITH PORT
-        perror("getaddrinfo");
-        pthread_exit(NULL);
-    }
-
-    socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (socket_fd < 0) {
-        perror("socket");
-        pthread_exit(NULL);
-    }
-
-    while(true) {
-        char* message = (char*) malloc(256);
-        strcpy(message, "Hello from the peer with port ");
-        strcpy(message + sizeof("Hello from the peer with port "), result->ai_addr->sa_data);
-        sendto(socket_fd, message, strlen(message), 0, result->ai_addr, result->ai_addrlen);
-        sleep(5);
-    }
-    close(socket_fd);
-    freeaddrinfo(result);
-    pthread_exit(NULL);
 }
 
 void handle_error(const char* error_msg) {
     perror(error_msg);
     exit(EXIT_FAILURE);
+}
+
+int is_valid_ipv4_address(const char* address) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, address, &(sa.sin_addr));
+    return result == 1;
+}
+
+int is_valid_ipv6_address(const char* address) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET6, address, &(sa.sin_addr));
+    return result == 1;
 }
